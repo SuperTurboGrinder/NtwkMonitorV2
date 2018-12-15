c1141ae1ddc5794ee46c373d8378150c2bccfafc
SuperTurbo <superturbogrinder@gmail.com>
2018-12-15 19:41:39 +0300

    Made electron app single instance only; fixed positioning bug for mut block component on monitor filter view

--- NativeClient/Electron/src/main.ts
+++ NativeClient/Electron/src/main.ts
@@ -1,22 +1,33 @@
-import { powerSaveBlocker, app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, powerSaveBlocker } from 'electron';
 import { join } from 'path';
 import { WebAPIProcessService } from './webAPIProcessService';
 
+let electronWindow: BrowserWindow = null;
+
+const shouldQuit = app.makeSingleInstance((_1, _2) => {
+  if (electronWindow) {
+    if (electronWindow.isMinimized()) electronWindow.restore();
+    electronWindow.focus();
+  }
+});
+
+if (shouldQuit) {
+  app.quit();
+}
+
 const monitorAPIStarter = new WebAPIProcessService();
-
-
 app.once('window-all-closed', () => app.quit());
 app.once('ready', () => {
     monitorAPIStarter.execAfterAPIStarted((ok: boolean) => {
         if (ok === true) {
             console.log('Starting electron app.');
-            let window = new BrowserWindow({
+            electronWindow = new BrowserWindow({
                 minWidth: 800,
                 minHeight: 600
             });
-            window.maximize();
-            window.once('closed', () => { window = null; });
-            window.loadURL('file://' + join(__dirname, '../index.html'));
+            electronWindow.maximize();
+            electronWindow.once('closed', () => { electronWindow = null; });
+            electronWindow.loadURL('file://' + join(__dirname, '../index.html'));
             // window.webContents.openDevTools();
             powerSaveBlocker.start('prevent-app-suspension');
         } else {
